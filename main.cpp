f778e0459330c86aa6b6f33c0739f4f48fca5bd7
eversonott <everson.ott@gmail.com>
2017-03-21 16:29:36 -0300

    acréscimo de uma função que faz o rastreio dos links em um laço

--- main.cpp
+++ main.cpp
@@ -21,35 +21,18 @@
 //svm
 using namespace std;
 
-size_t func(void* dados, size_t size, size_t nemb, string *saida){
-    saida->append( (char*)dados, size*nemb );
 
-    return size*nemb;
-}
-string getHTML(const char* url){
+void proximoLink(vector<string> * lista, QWebPage *p){
 
-    std::string STR;
-    CURL *curl= curl_easy_init();
+    p->mainFrame()->load(QUrl(lista->front().c_str()));
+    for( auto l : p->mainFrame()->findAllElements("a")){
+        lista->push_back(l.attribute("href").toStdString());
+    }
+    lista->erase(lista->begin());
+    //proximoLink(lista,p);
 
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, func);
-    curl_easy_setopt(curl,CURLOPT_WRITEDATA, &STR);
-
-    curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
-
-
-    return STR;
 }
 
-
-//Qlist<QString>* listaUrl;
-
-vector<string> lista;
-/*void proxima(auto p){
-    p.mainFrame()->load(QUrl("https://www.google.com.br"))
-}
-*/
 
 int main(int argc, char *argv[]) {
 
@@ -57,14 +40,13 @@ int main(int argc, char *argv[]) {
     string raiz ="https://www.google.com.br";
 
     lista.push_back(raiz);
+    QApplication app(argc, argv);
 
-        while(!lista.empty()) {
-            QApplication app(argc, argv);
+
 
             QWebPage p;
             p.mainFrame()->load(QUrl(lista.front().c_str()));
             lista.erase(lista.begin());
-
             if (p.mainFrame() == NULL) {
                 cout << "null";
             }
@@ -77,96 +59,32 @@ int main(int argc, char *argv[]) {
 
             QObject::connect(&p, &QWebPage::loadFinished, [&lista, &p](bool acabou) {
 
-                //cout << p.mainFrame()->toPlainText().toStdString();
                 for (auto l : p.mainFrame()->findAllElements("a")) {
-                    //cout << l.attribute("href").toStdString() << endl;
-
                     lista.push_back(l.attribute("href").toStdString());
-
-
                 }
-                //cout<<lista.front();
-                //cout<<lista.size();
 
+                proximoLink(&lista,&p);
                 for (int i = 0; i < lista.size(); i++) {
 
-                    cout << lista.at(i) << endl;
+                    cout<<lista.at(i)<< endl;
 
                 }
-                cout<<lista.size();
 
-                //cout << "teste" << p.mainFrame()->toHtml().toStdString() << "\n";
                 fflush(stdout);
-                //proxima();
+
 
             });
 
-            //proxima();
+
+
 
 
 
             //p.mainFrame()->findAllElements("a");
 
-            return app.exec();
-        }
 
 
 
-}
-
-int main2() {
-
-
-
-    vector<string> listaUrls;
-
-
-    //listaUrls.push_back("http://portal.fei.edu.br/pt-br/paginas/home.aspx");
-    string raizSite = "https://pt.wikipedia.org/wiki/Radioatividade";
-    listaUrls.push_back(raizSite);
-
-
-    while( !listaUrls.empty() ) {
-        string resultado = getHTML(listaUrls.front().c_str());
-        listaUrls.erase(listaUrls.begin());
-
-        QString raiz = "https://pt.wikipedia.org/wiki/Radioatividade";
-
-
-        QString array(resultado.c_str());
-
-        
-
-        QDomDocument d("doc");
-
-
-        if (!d.setContent(array)) {
-            cout << "Erro de parsing" << endl;
-
-        }
-
-        QDomNodeList nodes = d.elementsByTagName("a");
-        for (int i = 0; i < nodes.count(); i++) {
-            QDomNode elm = nodes.at(i);
-            if (elm.isElement()) {
-                QString url = elm.toElement().attribute("href");
-
-
-                if(url.startsWith("/") && !url.startsWith("//")){
-                    url = raiz + url;
-                }
-
-
-                listaUrls.push_back(url.toStdString());
-                cout <<i<<" "<< url.toStdString()<<endl;
-
-            }
-
-        }
-        cout<<"Itens do vetor" <<listaUrls.size()<<endl;
-
-
-
-    }
+    return app.exec();
 
 }
