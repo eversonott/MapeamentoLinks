1bc8be1638897dce06b7ac0014b425a7abf4e405
eversonott <everson.ott@gmail.com>
2017-10-19 04:07:14 -0200

    acrescimo da biblioteca lemon para criar o grafo com os links gerados do crawler

--- main.cpp
+++ main.cpp
@@ -16,18 +16,42 @@
 #include <QWebFrame>
 #include <QUrl>
 #include <QtWidgets/QApplication>
-#include <QList>
+#include <lemon/list_graph.h>
+#include <lemon/concepts/graph.h>
+#include <map>
 
 //svm
+
 using namespace std;
+using namespace lemon;
+
+void processaPagina(QWebPage &p, vector<string> &lista, map<string, ListDigraph::Node> &mapLista, ListDigraph &g) {
+    string fromUrl = p.mainFrame()->url().toString().toStdString();
+    if (mapLista.find(fromUrl) == mapLista.end()) {
+        mapLista[fromUrl] = g.addNode();
+    }
+    ListDigraph::Node noFrom = mapLista[fromUrl];
+
+    for (auto l : p.mainFrame()->findAllElements("a")) {
+        string toUrl = l.attribute("href").toStdString();
+        lista.push_back(toUrl);
 
 
-void proximoLink(vector<string> * lista, QWebPage *p){
+        if (mapLista.find(toUrl) == mapLista.end()) {
+            mapLista[toUrl] = g.addNode();
+        }
+
+        ListDigraph::Node noTo = mapLista[toUrl];
+        g.addArc(noFrom, noTo);
+
+    }
+
+}
+
+void proximoLink(vector<string> *lista, QWebPage *p, map<string, ListDigraph::Node> *mapLista, ListDigraph *g) {
+
 
     p->mainFrame()->load(QUrl(lista->front().c_str()));
-    for( auto l : p->mainFrame()->findAllElements("a")){
-        lista->push_back(l.attribute("href").toStdString());
-    }
     lista->erase(lista->begin());
     //proximoLink(lista,p);
 
@@ -37,53 +61,48 @@ void proximoLink(vector<string> * lista, QWebPage *p){
 int main(int argc, char *argv[]) {
 
     vector<string> lista;
-    string raiz ="https://www.google.com.br";
+    ListDigraph g;
+    map<string, ListDigraph::Node> mapLista;
+    //google = 1034 nós
+    //google = 1051 nós e 2341 arcos
+    string raiz = "https://www.google.com.br";
 
     lista.push_back(raiz);
+
     QApplication app(argc, argv);
 
 
-
-            QWebPage p;
-            p.mainFrame()->load(QUrl(lista.front().c_str()));
-            lista.erase(lista.begin());
-            if (p.mainFrame() == NULL) {
-                cout << "null";
-            }
-
-            QObject::connect(&p, &QWebPage::loadProgress, [](int i) {
-                cout << "progresso " << i << "\n";
-                fflush(stdout);
-            });
+    QWebPage p;
 
 
-            QObject::connect(&p, &QWebPage::loadFinished, [&lista, &p](bool acabou) {
-
-                for (auto l : p.mainFrame()->findAllElements("a")) {
-                    lista.push_back(l.attribute("href").toStdString());
-                }
-
-                proximoLink(&lista,&p);
-                for (int i = 0; i < lista.size(); i++) {
-
-                    cout<<lista.at(i)<< endl;
-
-                }
-
-                fflush(stdout);
+    p.mainFrame()->load(QUrl(lista.front().c_str()));
+    lista.erase(lista.begin());
 
 
-            });
+    if (p.mainFrame() == NULL) {
+        cout << "null";
+    }
+
+    QObject::connect(&p, &QWebPage::loadProgress, [](int i) {
+        //cout << "progresso " << i << "\n";
+        //fflush(stdout);
+    });
 
 
+    QObject::connect(&p, &QWebPage::loadFinished, [&lista, &p, &g, &mapLista](bool acabou) {
 
+        processaPagina(p, lista, mapLista, g);
 
+        for (int i = 0; i < lista.size(); i++) {
+            cout << lista.at(i) << endl;
+        }
 
-
-            //p.mainFrame()->findAllElements("a");
-
-
-
+        cout <<"nos"<< lemon::countNodes(g) << endl;
+        cout <<"arcos"<< lemon::countArcs(g)<<endl;
+        fflush(stdout);
+        p.mainFrame()->load(QUrl(lista.front().c_str()));
+        lista.erase(lista.begin());
+    });
 
     return app.exec();
 
